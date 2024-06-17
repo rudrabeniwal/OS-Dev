34c93d51f3d65cab96083773b855245e72e57377
Rudra Beniwal <rudrabeniwal@icloud.com>
2024-06-17 12:01:39 +0530

    loading loader file in memory

--- boot.asm
+++ boot.asm
@@ -9,14 +9,30 @@ start:
     mov sp,0x7c00
 
 TestDiskExtension:
-    mov [DriveId], dl
-    mov ah, 0x41
-    mov bx, 0x55aa
+    mov [DriveId], dl   ; Save drive ID (passed by BIOS in DL) to variable
+    mov ah, 0x41        ; Function 0x41 - Check for Extensions Supported
+    mov bx, 0x55aa      ; Magic number required by the BIOS for the function
+    int 0x13            ; BIOS disk services
+    jc NotSupport       ; If carry flag is set, extensions are not supported
+    cmp bx, 0xaa55      ; Check if BIOS returned the magic number
+    jne NotSupport      ; If not, extensions are not supported
+LoadLoader:
+    mov si, ReadPacket
+    mov word[si], 0x10 ; The first two bytes of the packet are set to 0x10, indicating the size of the packet (16 bytes)
+    mov word[si+2], 5 ;The next two bytes specify the number of sectors to read (5 sectors)
+    mov word[si+4], 0x7e00  ; Buffer address (segment:offset -> 0000:7e00)
+    mov word[si+6], 0       ; Buffer segment (0 in this case, using real mode segment:offset addressing)
+    mov dword[si+8], 1      ; Starting LBA (Logical Block Address) of the read
+    mov dword[si+0xc], 0    ; Reserved, should be zero
+    mov dl, [DriveId]
+    mov ah, 0x42
     int 0x13
-    jc NotSupport
-    cmp bx, 0xaa55
-    jne NotSupport
-PrintMessage:
+    jc ReadError
+
+    mov dl, [DriveId]
+    jmp 0x7e00
+ReadError:
+NotSupport:
     mov ah,0x13
     mov al,1
     mov bx,0xa
@@ -25,15 +41,15 @@ PrintMessage:
     mov cx,MessageLen 
     int 0x10
 
-NotSupport:
 End:
     hlt    
     jmp End
 
 DriveId: db 0
      
-Message:    db "Disk extension is supported!"
+Message:    db "We have error in boot process"
 MessageLen: equ $-Message
+ReadPacket: times 16 db 0
 
 times (0x1be-($-$$)) db 0
 
